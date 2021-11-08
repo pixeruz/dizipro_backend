615677feab19242e0df7cf4b829ee0f6c2eea487
muhammadyunusuz <yunuzmuhammad1@gmail.com>
2021-11-08 20:19:58 +0500

    add country model

--- modules/pg/pg.js
+++ modules/pg/pg.js
@@ -1,4 +1,5 @@
 const { Sequelize } = require("sequelize");
+const CountryModel = require("../../models/CountryModel");
 
 if (!process.env.PG_CONNECTION_URL) {
 	throw new Error("PG CONNECTION STRING NOT FOUND");
@@ -13,6 +14,10 @@ module.exports = async function pg() {
 		await sequelize.authenticate();
 
 		let db = {};
+
+		db.countries = await CountryModel(sequelize, Sequelize);
+
+		await sequelize.sync();
 
 		return db;
 	} catch (error) {
