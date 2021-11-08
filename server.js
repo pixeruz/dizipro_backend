3fff18ee360e27626b2372a9f1bed8add5c653f9
muhammadyunusuz <yunuzmuhammad1@gmail.com>
2021-11-08 20:05:21 +0500

    Add home router

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const { databaseMiddleware } = require("./middlewares/databaseMiddleware");
+const Routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -10,6 +11,7 @@ async function server() {
 		app.use(express.json());
 		app.use(express.urlencoded({ extended: true }));
 		app.use(databaseMiddleware);
+		app.use("/v1", Routes);
 	} catch (error) {
 		console.log("SERVER_ERROR:", error);
 	}
