84ec57b8be9b3b48f4b1670ce15a268f7b3046d9
muhammadyunusuz <yunuzmuhammad1@gmail.com>
2021-11-10 20:08:49 +0500

    add auth middleware

--- routes/HomeRoute.js
+++ routes/HomeRoute.js
@@ -1,6 +1,9 @@
 const { HomeGetController } = require("../controllers/HomeController");
+const AuthMiddleware = require("../middlewares/AuthMiddleware");
 
 const HomeRouter = require("express").Router();
+
+HomeRouter.use(AuthMiddleware);
 
 HomeRouter.get("/", HomeGetController);
 
