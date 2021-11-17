c7fe03daead742eec3a6c009c2c0ec1ddd59d1c1
muhammadyunusuz <yunuzmuhammad1@gmail.com>
2021-11-17 19:49:34 +0500

    add software controller and router

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,14 @@ const AdminRouter = require("./AdminRoute");
 const CountyRouter = require("./CountryRoute");
 const HomeRouter = require("./HomeRoute");
 const SkillRouter = require("./SkillRoute");
+const SoftwareRouter = require("./SoftwareRoute");
 const UserRouter = require("./UserRoute");
 
 Router.use("/countries", CountyRouter);
 Router.use("/users", UserRouter);
 Router.use("/admin", AdminRouter);
 Router.use("/skills", SkillRouter);
+Router.use("/softwares", SoftwareRouter);
 Router.use("/", HomeRouter);
 
 module.exports = Router;
