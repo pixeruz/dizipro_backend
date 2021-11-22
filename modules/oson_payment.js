3e85f18ee879535e40db94f3d92a34589cffd33b
muhammadyunusuz <yunuzmuhammad1@gmail.com>
2021-11-22 19:16:39 +0500

    add oson api

--- modules/oson_payment.js
+++ modules/oson_payment.js
@@ -39,25 +39,24 @@ class OSON_API {
 
 		return response;
 	}
+
+	async checkInvoice(transaction_id) {
+		let response = await axios({
+			url: "https://api.oson.uz/api/invoice/status",
+			headers: {
+				"Content-Type": "application/json",
+				Accept: "application/json",
+				token: this.API_KEY,
+			},
+			method: "POST",
+			data: {
+				merchant_id: 1474,
+				transaction_id,
+			},
+		});
+
+		return response;
+	}
 }
 
-async function test() {
-	const oson = new OSON_API(process.env.OSON_AUTH_TOKEN);
-
-	let x = await oson.createInvoice(
-		"112",
-		"2000",
-		"UZS",
-		"muhammadyunusuz@yandex.com",
-		"Test uchun",
-		"https://muhammadyunus.uz",
-		30,
-		"en"
-	);
-
-	x = await x.data;
-
-	console.log(x);
-}
-
-test();
+module.exports = OSON_API;
